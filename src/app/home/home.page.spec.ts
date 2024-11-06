c2d88906d85919902c64823ee74bf32a7cfaf6fc
Giovanni Gonzalez <gi.gonzalezm@duocuc.cl>
2024-11-06 11:59:38 -0300

    Clase 06-11-2024 Pruebas Unitarias

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import { FormsModule } from '@angular/forms';
 
 describe('HomePage', () => {
   let component: HomePage;
@@ -10,7 +11,7 @@ describe('HomePage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomePage],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(),FormsModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
@@ -30,5 +31,13 @@ describe('HomePage', () => {
   it('texto en mayusculas de manera correcta', () => {
     const resultado = component.toMayuscula('hola');
     expect(resultado).toEqual('HOLA');
+  }); 
+
+  it('Validacion Del Campo Nombre Cuando Esta Vacio', () => {
+    component.name = "";
+    component.age = 14;
+    component.onValidate();
+
+    expect(component.errorMessage).toBe("Debe rellenar el campo");
   }); 
 });
